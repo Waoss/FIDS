659e2c9dece501678a15044f697bf9057025f84c
rahul29112002@gmail.com <rahul29112002@gmail.com>
2017-06-13 17:07:20 +0530

    Created Main class' test

---
+++ src/test/java/com/waoss/util/fids/FIDSTest.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the GNU General Public License 3.0.
+ * Copyright Waoss and its affliates.
+ * This library ,hereby permits :
+ * 1. Commercial Use
+ * 2. Modification
+ * 3. Distribution
+ * 4. Patent Use
+ * 5. Private Use
+ * Under the following conditions :
+ * 1. License and copyright notice
+ * 2. State changes
+ * 3. Disclose source
+ * 4. Same license
+ * With the limitations of no liability or warranty.
+ * For more information, see LICENSE.MD
+ */
+
+package com.waoss.util.fids;
+
+import org.junit.Test;
+
+/**
+ * Created by RAHUL on 13-06-2017.
+ */
+public class FIDSTest {
+    @Test
+    public void main() throws Exception {
+        FIDS.main(new String[]{
+                "asdasdasdsadasd"
+        });
+    }
+
+}
